6d8f616a8308ab8f02904a0177bc29b7a68426fe
szelda64 <84426635+szelda64@users.noreply.github.com>
2025-02-20 23:12:13 -0500

    first attempt at making graph generators

--- genGraphs.py
+++ genGraphs.py
@@ -16,6 +16,7 @@ def genGraph1(n):
             if i != j and math.log2(math.abs(i-j)).is_integer():
                 graph[(i,j)] = graph[(j,i)] = np.random.rand()
     return graph
+
 def genGraph2(n):
     graph = {}
     x_values = []
@@ -28,9 +29,35 @@ def genGraph2(n):
             if i != j:
                 graph[(i,j)] = graph[(j,i)] = math.sqrt((math.abs(x_values[i-1]-x_values[j-1]) ** 2) + (math.abs(y_values[i-1] - y_values[j-1]) ** 2))
     return graph
+
 def genGraph3(n):
-    return 0
+    graph = {}
+    x_values = []
+    y_values = []
+    z_values = []
+    for _ in range(1, n+1):
+        x_values.append(np.random.rand())
+        y_values.append(np.random.rand())
+        z_values.append(np.random.rand())
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if i != j:
+                graph[(i,j)] = graph[(j,i)] = math.sqrt((math.abs(x_values[i-1]-x_values[j-1]) ** 2) + (math.abs(y_values[i-1] - y_values[j-1]) ** 2)+(math.abs(z_values[i-1]-y_values[j-1]) ** 2))
+    return graph
+
 def genGraph4(n):
-    return 0
+    graph = {}
+    x_values = []
+    y_values = []
+    z_values = []
+    for _ in range(1, n+1):
+        x_values.append(np.random.rand())
+        y_values.append(np.random.rand())
+        z_values.append(np.random.rand())
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if i != j and math.log2(math.abs(i-j)).is_integer():
+                graph[(i,j)] = graph[(j,i)] = math.sqrt((math.abs(x_values[i-1]-x_values[j-1]) ** 2) + (math.abs(y_values[i-1] - y_values[j-1]) ** 2)+(math.abs(z_values[i-1]-y_values[j-1]) ** 2))
+    return graph
 
 
