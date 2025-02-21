a2d23692e3d617ac1173eb8e72d623d856279bcc
szelda64 <84426635+szelda64@users.noreply.github.com>
2025-02-20 23:09:48 -0500

    hiii

--- genGraphs.py
+++ genGraphs.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 def genGraph0(n):
     graph = {}
 
@@ -9,9 +10,24 @@ def genGraph0(n):
     return graph
 
 def genGraph1(n):
-    return 0
+    graph = {}
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if i != j and math.log2(math.abs(i-j)).is_integer():
+                graph[(i,j)] = graph[(j,i)] = np.random.rand()
+    return graph
 def genGraph2(n):
-    return 0
+    graph = {}
+    x_values = []
+    y_values = []
+    for _ in range(1, n+1):
+        x_values.append(np.random.rand())
+        y_values.append(np.random.rand())
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if i != j:
+                graph[(i,j)] = graph[(j,i)] = math.sqrt((math.abs(x_values[i-1]-x_values[j-1]) ** 2) + (math.abs(y_values[i-1] - y_values[j-1]) ** 2))
+    return graph
 def genGraph3(n):
     return 0
 def genGraph4(n):
