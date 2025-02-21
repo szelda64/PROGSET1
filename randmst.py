9a9f6c950c20b9bef28e0aa5a185332625d0f437
szelda64 <84426635+szelda64@users.noreply.github.com>
2025-02-20 22:49:22 -0500

    aaa 2

--- randmst.py
+++ randmst.py
@@ -5,5 +5,9 @@ if(init.flag == 0):
     graph = genGraph0(init.nump)
 if(init.flag == 1):
     graph = genGraph1(init.nump)
-if(init.flag == 1):
-    graph = genGraph1(init.nump)
+if(init.flag == 2):
+    graph = genGraph2(init.nump)
+if(init.flag == 3):
+    graph = genGraph3(init.nump)
+if(init.flag == 4):
+    graph = genGraph4(init.nump)
