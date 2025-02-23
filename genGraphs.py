135582dd65369fef20a8e173865e5392c2fae1ee
szelda64 <84426635+szelda64@users.noreply.github.com>
2025-02-23 14:33:57 -0500

    hi marky

--- genGraphs.py
+++ genGraphs.py
@@ -1,24 +1,24 @@
 import numpy as np
 import math
 def genGraph0(n):
-    graph = {}
+    graph = []
 
     for i in range(1,n+1):
         for j in range(1, n+1):
             if i != j:
-                graph[(i,j)] = np.random.rand()
+                graph.append(i,j,np.random.rand())
     return graph
 
 def genGraph1(n):
-    graph = {}
+    graph = []
     for i in range(1, n+1):
         for j in range(1, n+1):
             if i != j and math.log2(math.abs(i-j)).is_integer():
-                graph[(i,j)] = graph[(j,i)] = np.random.rand()
+                graph.append(i,j,np.random.rand())
     return graph
 
 def genGraph2(n):
-    graph = {}
+    graph = []
     x_values = []
     y_values = []
     for _ in range(1, n+1):
@@ -27,11 +27,11 @@ def genGraph2(n):
     for i in range(1, n+1):
         for j in range(1, n+1):
             if i != j:
-                graph[(i,j)] = graph[(j,i)] = math.sqrt((math.abs(x_values[i-1]-x_values[j-1]) ** 2) + (math.abs(y_values[i-1] - y_values[j-1]) ** 2))
+                graph.append(i,j,math.sqrt((math.abs(x_values[i-1]-x_values[j-1]) ** 2) + (math.abs(y_values[i-1] - y_values[j-1]) ** 2)))
     return graph
 
 def genGraph3(n):
-    graph = {}
+    graph = []
     x_values = []
     y_values = []
     z_values = []
@@ -42,11 +42,11 @@ def genGraph3(n):
     for i in range(1, n+1):
         for j in range(1, n+1):
             if i != j:
-                graph[(i,j)] = graph[(j,i)] = math.sqrt((math.abs(x_values[i-1]-x_values[j-1]) ** 2) + (math.abs(y_values[i-1] - y_values[j-1]) ** 2)+(math.abs(z_values[i-1]-y_values[j-1]) ** 2))
+                graph.append(i,j,math.sqrt((math.abs(x_values[i-1]-x_values[j-1]) ** 2) + (math.abs(y_values[i-1] - y_values[j-1]) ** 2)+(math.abs(z_values[i-1]-y_values[j-1]) ** 2)))
     return graph
 
 def genGraph4(n):
-    graph = {}
+    graph = []
     x_values = []
     y_values = []
     z_values = []
@@ -57,7 +57,7 @@ def genGraph4(n):
     for i in range(1, n+1):
         for j in range(1, n+1):
             if i != j and math.log2(math.abs(i-j)).is_integer():
-                graph[(i,j)] = graph[(j,i)] = math.sqrt((math.abs(x_values[i-1]-x_values[j-1]) ** 2) + (math.abs(y_values[i-1] - y_values[j-1]) ** 2)+(math.abs(z_values[i-1]-y_values[j-1]) ** 2))
+                graph.append(i,j, math.sqrt((math.abs(x_values[i-1]-x_values[j-1]) ** 2) + (math.abs(y_values[i-1] - y_values[j-1]) ** 2)+(math.abs(z_values[i-1]-y_values[j-1]) ** 2)))
     return graph
 
 
