48a6fc4397d359e7c6e89e9c55521dadd91b10da
KingMeMeMe <112827546+KingMeMeMe@users.noreply.github.com>
2025-02-27 19:41:38 -0500

    grahhghhhh2

--- prims_algo.py
+++ prims_algo.py
@@ -106,7 +106,7 @@ def prims_algo(graph, source):
     verts = graph[0]
     print(verts, "with length", len(verts))
     edges = graph[1] 
-    #print('Edges:', edges, "\n")
+    print('Edges:', edges, "\n")
 
     #initialize all distances to inf, aside from source
     dist = [math.inf]*len(verts)
@@ -116,7 +116,7 @@ def prims_algo(graph, source):
     mst = []
     heap_graph = Heap()
 
-    heap_graph.insert([source, 0])
+    heap_graph.insert(source)
 
     while heap_graph:
         u = heap_graph.extractMin()
@@ -127,24 +127,20 @@ def prims_algo(graph, source):
 
         #e = ((start, end), weight)
         #e[0] = (start, end)
-        unvisitedAdjE = []
+        #for (u,v) in Edges
+        adjEdges = []
         for e in edges:
-            #for (u,v) in Edges
             if e[0][0] == u:
-                print(e[0])
-                #for v not in mst
-                mstv = [item[0] for item in mst]
-                if e[0][1] not in mstv:
-                    unvisitedAdjE.append(e)
-        print('Unvisited:', unvisitedAdjE)
+                adjEdges.append(e)
+        print(adjEdges)
 
-        for e in unvisitedAdjE:
-            #if dist[v] > weight
-            print("Current edge:", e)
-            if dist[e[0][1]] > e[1]:
-                dist[e[0][1]] = e[1]
-                prev[e[0][1]] = u
-                heap_graph.insert(e[0][1])
+        for e in adjEdges:
+            if e[0][1] not in mst:
+                #if dist[v] > weight
+                if dist[e[0][1]] > e[1]:
+                    dist[e[0][1]] = e[1]
+                    prev[e[0][1]] = u
+                    heap_graph.insert(e[0][1])
 
     return (dist, prev)
 
