8e0a0b7a623d675c3a114cb26417d32f090bc464
KingMeMeMe <112827546+KingMeMeMe@users.noreply.github.com>
2025-03-02 00:55:35 -0500

    primmy mwah mwah mwah love u zekey

--- rehualPrim.py
+++ rehualPrim.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-from genGraphs import genCompGraph, genHyperCube, genGeoCompGraph, genGeoCubeGraph, genGeoHyperCube
+from genGraphRehaul import Graph, genCompGraph, genHyperCube, genGeoCompGraph, genGeoCubeGraph, genGeoHyperCube
 
 class Heap:
     def __init__(self):
@@ -73,78 +73,79 @@ class Heap:
     
 
 def prims_algo(graph, source):
-    #graph = [[list of vertices], [list of ((start, end), weight)]]
-    verts = graph[0]
-    #print("Vertices: " + str(verts), "with length", len(verts))
-    edges = graph[1]
-    #print("Edges: " + str(edges), "with length", len(edges)) 
+    verts = graph.verts
+    print("Vertices:", verts)
+    edges = graph.edges
+    print("Edges: " + str(edges), "with length", len(edges)) 
 
     #initialize all distances to inf, aside from source
-    dist = [math.inf]*len(verts)
-    prev = [None]*len(verts)
+    dist = [math.inf]*verts
     dist[source] = 0
+    prev = [-1]*verts
+    print(dist)
+    print(prev)
     
     mst = []
     heap_graph = Heap()
 
     heap_graph.insert([source, 0])
 
+    print(heap_graph.array)
+
     while heap_graph.array:
-        #print("Current heap: " + str(heap_graph.array))
+        #u = [vertex label, vertex value]
         u = heap_graph.extractMin()
-        #print('min:', u)
-        #print('current array', heap_graph.array)
-        mst.append(u)
-        #print('mst:', mst)
 
-        #e = ((start, end), weight)
-        #e[0] = (start, end)
-        unvisitedAdjE = []
-        for e in edges:
-            #for (u,v) in Edges
-            if e[0][0] == u[0]:
-                #for v not in mst
-                mstv = [item[0] for item in mst]
-                if e[0][1] not in mstv:
-                    unvisitedAdjE.append(e)
-        #print('Unvisited:', unvisitedAdjE)
+        if u not in mst:
+            mst.append(u)
+            
+            for i in range(verts):
+                if i in graph.edges[u]:
+                    if i not in mst and graph.edges[u][i] > dist[i]:
+                        dist[i] = graph.edges[u][i]
+                        prev[i] = u
+                        heap_graph.insert([i, dist[i]])
 
-        for e in unvisitedAdjE:
-            #if dist[v] > weight
-            if dist[e[0][1]] > e[1]:
-                dist[e[0][1]] = e[1]
-                prev[e[0][1]] = u
-                #print([e[0][1], e[1]])
-                heap_graph.insert([e[0][1], e[1]])
+    return mst
 
-    return (dist, prev)
 
-    # heap_graph = heapConstruct(graph)
-    # pqueue = {}
-    # s = source
-    # keylist = list(heap_graph.keys())
-    # ed = keylist
-    # vertices = []
-    # while ed:
-    #     if(ed[0][0] not in vertices):
-    #         vertices.append([0][0])
-    #         ed.pop(0)
-    #     if(ed[0][1] not in vertices):
-    #         vertices.append([0][1])
-    #         ed.pop(0)
 
-    # dist = {vertex: float('infinity') for vertex in vertices}
-    # dist[s] = 0
-    # mst_included = {vertex: False for vertex in vertices}
-    # while pqueue:
-    #     curr_key = list(pqueue.keys)[0]
-    #     curr_vert = curr_key[0]
-    #     mst_included[curr_vert] = True
-    #     for key, value in heap_graph():
-    #         if key[0] not in mst_included.keys() and key[1] == curr_key[1] and dist[key[0]] > value:
-    #             dist[key[0]] = value
-    #             heap_graph.append(key, dist[key[0]])
-    # return heap_graph
+
+    # while heap_graph.array:
+    #     #print("Current heap: " + str(heap_graph.array))
+    #     u = heap_graph.extractMin()
+    #     #print('min:', u)
+    #     #print('current array', heap_graph.array)
+    #     mst.append(u)
+    #     #print('mst:', mst)
+
+        
+
+
+
+        # #e = ((start, end), weight)
+        # #e[0] = (start, end)
+        # unvisitedAdjE = []
+        # for e in edges:
+        #     #for (u,v) in Edges
+        #     if e[0][0] == u[0]:
+        #         #for v not in mst
+        #         mstv = [item[0] for item in mst]
+        #         if e[0][1] not in mstv:
+        #             unvisitedAdjE.append(e)
+        # #print('Unvisited:', unvisitedAdjE)
+
+    #     for e in unvisitedAdjE:
+    #         #if dist[v] > weight
+    #         if dist[e[0][1]] > e[1]:
+    #             dist[e[0][1]] = e[1]
+    #             prev[e[0][1]] = u
+    #             #print([e[0][1], e[1]])
+    #             heap_graph.insert([e[0][1], e[1]])
+
+    # return (dist, prev)
+
+    
 
 def count_weight(graph):
     #print("\nRunning count weight on graph...")
