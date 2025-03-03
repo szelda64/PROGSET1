23b11403b77d199b7577829c9d9419d2318170ae
szelda64 <84426635+szelda64@users.noreply.github.com>
2025-03-02 23:51:39 -0500

    simpler

--- randmst.py
+++ randmst.py
@@ -292,30 +292,10 @@ def randomSample(numpoints, numtrials, dimension):
     #print("Incorrect number of command lines. randmst terminal usage is: \n")
     #print("python randmst.py [flag] [numpoints] [numtrials] [dimension]")
     #sys.exit()
-try:
-    global flag
-    flag = int(sys.argv[1])
-except ValueError:
-    print("flag must be an integer.")
-    sys.exit()
-try:
-    global nump
-    nump = int(sys.argv[2])
-except ValueError:
-    print("numpoints must be an integer.")
-    sys.exit()
-try:
-    global numt
-    numt = int(sys.argv[3])
-except ValueError:
-    print("numtrials must be an integer.")
-    sys.exit()
-try:
-    global dim
-    dim = int(sys.argv[4])
-except ValueError:
-    print("dimension must be an integer.")
-    sys.exit()
+flag = int(sys.argv[1])
+nump = int(sys.argv[2])
+numt = int(sys.argv[3])
+dim = int(sys.argv[4])
 #start = time.time()
 randomSample(nump,numt,dim)
 #stop = time.time() - start
