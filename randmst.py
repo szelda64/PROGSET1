b3e79ad886d50a1eca8f6fb2c9e2ee4e02ddef3a
szelda64 <84426635+szelda64@users.noreply.github.com>
2025-03-03 20:54:30 -0500

    bwuh

--- randmst.py
+++ randmst.py
@@ -2,6 +2,135 @@ import sys
 import numpy as np
 import math
 
+class AMGraph:
+
+    def __init__(self, n, edges = None):
+        self.verts = n
+        self.edges = [dict() for _ in range(n)]
+
+    def add_edge(self, u, v, w):
+        if v not in self.edges[u] and u not in self.edges[v]:
+            self.edges[u][v] = w
+            self.edges[v][u] = w
+        return self
+
+class Heap:
+    def __init__(self):
+        self.array = []
+
+    def parent(self, v):
+        return (v-1) // 2
+    def leftchild(self, v):
+        return 2 * v + 1
+    def rightchild(self, v):
+        return 2 * v + 2
+
+    def insert(self, v):
+        #print("\nCurrently running insert on heap for element: " + str(v))
+        self.array.append(v)
+        #print("Current heap: " + str(self.array))
+        n = len(self.array) - 1
+        #print("Index of last element: " + str(n))
+        while (n > 0) and self.array[self.parent(n)][1] > self.array[n][1]:
+            #print('self.array[self.parent(n)][1]:',self.array[self.parent(n)][1])
+            #print('self.array[n][1]:',self.array[n][1])
+            self.array[self.parent(n)], self.array[n] = self.array[n], self.array[self.parent(n)]
+            n = self.parent(n)
+        #print("Current heap: " + str(self.array),'\n')
+
+    #the problem is here
+    def minHeapify(self, v):
+        #print("\nCurrently running minHeapify on heap for element: " + str(v))
+        #print("Current array",self.array)
+        n = self.array.index(v)
+        #print("Position of element in array: " + str(n))
+        smallest = n
+        l,r = self.leftchild(n), self.rightchild(n)
+        #print(n,"has children", l, "and", r)
+        #print('Current heap:', self.array)
+        if l < len(self.array): 
+            if self.array[l] < self.array[n]:
+                smallest = l
+        else:
+            smallest = n
+
+        if r < len(self.array):
+            if self.array[r] < self.array[smallest]:
+                smallest = r
+        #print('Smallest:', smallest, "\n")
+        if smallest != n: 
+            self.array[n], self.array[smallest] = self.array[smallest], self.array[n]
+            self.minHeapify(self.array[smallest])
+
+    def buildHeap(self):
+        for i in range(len(self.array)/2, 1, -1):
+            self.minHeapify(self, i)
+
+    def extractMin(self):
+        #print("\nCurrently running extractMin on heap")
+        #print("Array before: " + str(self.array))
+        min = self.array[0]
+        self.array[0] = self.array[-1]
+        self.array.pop()
+        #print("Array after popping: " + str(self.array))
+        #print("Length of array after popping: " + str(len(self.array)))
+        if len(self.array) != 0:
+            self.minHeapify(self.array[0])
+        return min
+
+def prims_algo(graph, source):
+    verts = graph.verts
+    #print("Vertices:", verts)
+    edges = graph.edges
+    #print("Edges: " + str(edges), "with length", len(edges)) 
+
+    #initialize all distances to inf, aside from source
+    dist = [math.inf]*verts
+    dist[source] = 0
+    prev = [-1]*verts
+    #print(dist)
+    #print(prev)
+
+    mst = []
+    in_mst = [False]*verts
+    heap_graph = Heap()
+
+    heap_graph.insert([source, 0])
+
+    #print(heap_graph.array)
+
+    while heap_graph.array:
+        #u = [vertex label, vertex value]
+        u = heap_graph.extractMin()
+        #print('Min:', u)
+
+        if not in_mst[u[0]]:
+
+            mst.append(u)
+            in_mst[u[0]] = True
+
+            for i in range(verts):
+                #print("We check if", i, "is key in", edges[u[0]])
+                if i in edges[u[0]]:
+                    #print("Tis")
+                    #print(i, "has weight", graph.edges[u[0]][i])
+                    if i not in mst and graph.edges[u[0]][i] < dist[i]:
+                        dist[i] = graph.edges[u[0]][i]
+                        prev[i] = u[0]
+                        heap_graph.insert([i, dist[i]])
+            #print(heap_graph.array,'\n')
+
+    return mst
+
+def AMgenCompGraph(n):
+    g = AMGraph(n)
+    for i in range(n):
+        for j in range(n):
+            if i < j:
+                weight = np.random.rand()
+                if(cutoff(n,weight,0)):
+                    g.add_edge(i, j, weight)
+    return g
 
 def cutoff(n,weight,dim):
     if(n < 128):
@@ -9,7 +138,7 @@ def cutoff(n,weight,dim):
     if(dim == 0):
         return not(weight > 1/(n/3))
     if(dim == 1):
-        return not(weight > 1/(math.log2(n)/2))
+        return not(weight > 1/(math.log2(n)/n))
     if(dim == 2):
         return not(weight > 1/((n ** (1/2))/5))
     if(dim == 3):
@@ -265,7 +394,7 @@ def randomSample(numpoints, numtrials, dimension):
     curr_weight = 0
     for _ in range(numtrials):
         if(dimension == 0):
-            graph = ELgenCompGraph(numpoints)
+            graph = AMgenCompGraph(numpoints)
         if(dimension == 1):
             graph = ELgenHyperCube(numpoints)
         if(dimension == 2):
@@ -274,7 +403,10 @@ def randomSample(numpoints, numtrials, dimension):
             graph = ELgenGeoCubeGraph(numpoints)
         if(dimension == 4):
             graph = ELgenGeoHyperCube(numpoints)
-        curr_weight += count_weight(kruskals(graph))
+        if(dimension == 1):
+            curr_weight += count_weight(kruskals(graph))
+        else:
+            curr_weight += count_weight(prims_algo(graph,0))
     average = curr_weight / numtrials
     return average, numpoints, numtrials, dimension
 
